d6f5ba5a4ad3e4d2f807d611625cc1c8923603c4
Dmytro Gul <guldmytro@gmail.com>
2021-10-14 21:13:08 +0300

    CSS Desktop all pages

--- js/main.js
+++ js/main.js
@@ -34,3 +34,87 @@ $('.portfolio-slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1
 });
+
+// project slider
+$('.project-slider').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+// catalog slider
+$('.related-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 1
+});
+
+// catalog slider
+$('.related-slider_secondary').slick({
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 4,
+    slidesToScroll: 1
+});
+
+// product slider
+$('.product-main-gallery').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product-aside-gallery',
+    autoplay: true
+});
+
+$('.product-aside-gallery').slick({
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    vertical: true,
+    verticalSwiping: true,
+    cssEase: 'ease',
+    asNavFor: '.product-main-gallery',
+    responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+        breakpoint: 600,
+        settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+$('.product-aside-gallery .item').on('click', function(e) {
+    const index = $(this).attr('data-slick-index');
+    $('.product-main-gallery').slick('slickGoTo', index, false);
+});
